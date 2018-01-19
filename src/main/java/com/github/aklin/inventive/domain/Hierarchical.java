8d10991fba04525f8c31451d8bbce2e1dfd05ccf
Tony Klinakis <a.klinakis@yahoo.com>
2018-01-19 22:23:57 +0000

    Simplify getFullLocation and move to Hierarchical

--- src/main/java/com/github/aklin/inventive/domain/Hierarchical.java
+++ src/main/java/com/github/aklin/inventive/domain/Hierarchical.java
@@ -2,6 +2,9 @@ package com.github.aklin.inventive.domain;
 
 import com.fasterxml.jackson.annotation.JsonGetter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Represents an entity that exists in a parent-child hierarchy. A parent can have many children,
  * but a child can only have one parent.
@@ -15,8 +18,24 @@ public interface Hierarchical {
 	 */
 	public Hierarchical getParent();
 
+
+	/**
+	 * Get hierarchy path. The first element in the result is the root element. The last element is the current element.
+	 *
+	 * @return
+	 */
+	public default List<Hierarchical> getFullLocation() {
+		final List<Hierarchical> ret;
+
+		ret = isRoot() ? new ArrayList<>() : getParent().getFullLocation();
+
+		ret.add(this);
+
+		return ret;
+	}
+
 	@JsonGetter("is-root")
-	public default boolean  isRoot(){
+	public default boolean isRoot() {
 		return getParent() == null;
 	}
 }
--- src/main/java/com/github/aklin/inventive/domain/Locatable.java
+++ src/main/java/com/github/aklin/inventive/domain/Locatable.java
@@ -1,8 +1,5 @@
 package com.github.aklin.inventive.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Describes an immutable (ie. geographic) location
  */
@@ -15,26 +12,4 @@ public interface Locatable extends Identifiable, Hierarchical {
 	 */
 	@Override
 	public Locatable getParent();
-
-	/**
-	 * Get full path as list. Root location is the first element of the list. The last item in the list is this element.
-	 *
-	 * @return
-	 */
-	public default List<Locatable> getFullLocation() {
-		final List<Locatable> ret;
-
-		if (getParent() != null) {
-			ret = getParent().getFullLocation();
-			ret.add(getParent());
-			return ret;
-		}
-
-		ret = new ArrayList<>();
-
-		ret.add(this);
-
-		return ret;
-	}
-
 }
