a03aa826fd10be31818ede0743d13eec4fe2a0fa
Tony Klinakis <a.klinakis@yahoo.com>
2018-01-19 22:31:54 +0000

    Add items API controller

---
+++ src/main/java/com/github/aklin/inventive/controllers/api/v0/ItemAPIController.java
@@ -0,0 +1,27 @@
+package com.github.aklin.inventive.controllers.api.v0;
+
+import com.github.aklin.inventive.domain.InventoryEntry;
+import com.github.aklin.inventive.services.InventoryEntryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ItemAPIController {
+
+	private InventoryEntryService entryService;
+
+	@Autowired
+	public void setEntryService(InventoryEntryService entryService) {
+		this.entryService = entryService;
+	}
+
+	@RequestMapping("/api/v0/items")
+	public List<InventoryEntry> index() {
+		return entryService.listAll();
+	}
+}
+
+
