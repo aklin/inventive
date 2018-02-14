e0083aa74a1a0099ba0b533ebc2a192e20fd1ba2
Tony Klinakis <a.klinakis@yahoo.com>
2018-02-14 00:12:50 +0000

    Add login page
    Move config to own package
    Whitelist static resources

--- src/main/java/com/github/aklin/inventive/config/SecurityConfig.java
+++ src/main/java/com/github/aklin/inventive/config/SecurityConfig.java
@@ -1,9 +1,10 @@
-package com.github.aklin.inventive;
+package com.github.aklin.inventive.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -28,5 +29,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 				.withUser("user").password("password").roles("USER");
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/dist/**",
+				"/bower_components/**",
+				"/plugins/**");
 	}
 }
